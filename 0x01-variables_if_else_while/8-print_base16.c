e244cd2f5e62c1afc055f9cc157840e105d48fc5
KhanyisileN <dr_litshane@yahoo.com>
2023-03-20 18:32:21 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main -  prints all the numbers of base 16 in lowercase
+ * Return: (0);
+ */
+int main(void)
+{
+	int k;
+	char alp;
+
+	for (k = '0'; k < '9'; d++)
+	putchar(k);
+	for (alp = 'a'; alp <= 'f'; alp++)
+	putchar(alp);
+	putchar('\n');
+	return (0);
+}
