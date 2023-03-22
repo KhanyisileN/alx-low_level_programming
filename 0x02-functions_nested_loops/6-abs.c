11533641121880453e596ed69f3f3fec4d2a7289
KhanyisileN <dr_litshane@yahoo.com>
2023-03-22 16:38:55 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - write the absolute value on an integer
+ * @n: the number to be written
+ * Return: Absolute value of number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	int abs_val;
+
+	abs_val = n = -1;
+	return (abs_val);
+	}
+	return (n);
+}
