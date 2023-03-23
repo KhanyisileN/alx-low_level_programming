16f0f330ce270f4e1e90a2a02e7951e4500be621
KhanyisileN <dr_litshane@yahoo.com>
2023-03-22 18:15:59 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (; n<= 98; n++)
+	for (; n <= 98; n++)
 	{
 	if (n == 98)
 	{
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	for (; n>= 98; n--)
+	for (; n >= 98; n--)
 	{
 	if (n == 98)
 	{
@@ -35,7 +35,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d, ",n);
+	printf("%d, ", n);
 	}
 	}
 	}
